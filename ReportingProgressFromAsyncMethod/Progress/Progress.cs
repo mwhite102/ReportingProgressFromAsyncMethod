92315594c9880ca8388f2f8ed41567fc616916da
mwhite102 <mwhite102@gmail.com>
2017-01-28 11:37:25 -0600

    Implemented MainViewModel and progress reporting

---
+++ ReportingProgressFromAsyncMethod/Progress/Progress.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportingProgressFromAsyncMethod
+{
+    public class Progress
+    {
+        public int ProgressPercent { get; set; }
+        public string ProgressText { get; set; }
+    }
+}
--- ReportingProgressFromAsyncMethod/ViewModel/MainViewModel.cs
+++ ReportingProgressFromAsyncMethod/ViewModel/MainViewModel.cs
@@ -1,19 +1,14 @@
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System.Windows.Input;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace ReportingProgressFromAsyncMethod.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
     public class MainViewModel : ViewModelBase
     {
         /// <summary>
@@ -21,14 +16,172 @@ namespace ReportingProgressFromAsyncMethod.ViewModel
         /// </summary>
         public MainViewModel()
         {
-            ////if (IsInDesignMode)
-            ////{
-            ////    // Code runs in Blend --> create design time data.
-            ////}
-            ////else
-            ////{
-            ////    // Code runs "for real"
-            ////}
         }
+
+        private bool _IsBusy;
+
+        public bool IsBusy
+        {
+            get { return _IsBusy; }
+            set
+            {
+                if (_IsBusy != value)
+                {
+                    _IsBusy = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private int _ProgressPercent;
+
+        public int ProgressPercent
+        {
+            get { return _ProgressPercent; }
+            set
+            {
+                if (_ProgressPercent != value)
+                {
+                    _ProgressPercent = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        private string _ProgressText;
+
+        public string ProgressText
+        {
+            get { return _ProgressText; }
+            set
+            {
+                if (_ProgressText != value)
+                {
+                    _ProgressText = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        #region Private Methods
+
+        private void DoCancel()
+        {
+            //
+        }
+
+        private async void DoStart()
+        {
+            var progress = new Progress<Progress>(ReportProgress);
+            await DoLongRunningProcess(progress);
+        }
+
+        private async Task DoLongRunningProcess(IProgress<Progress> progress)
+        {
+            List<int> values = Enumerable.Range(1, 100).ToList();
+
+            int total = values.Count;
+
+            await Task.Run(() => {
+                if (progress != null)
+                {
+                    int cnt = 0;
+                    foreach(var v in values)
+                    {
+                        cnt++;
+                        progress.Report(new Progress()
+                        {
+                            ProgressPercent = ((cnt * 100 / total)),
+                            ProgressText = $"{cnt} of {total}"
+                        });
+                        Thread.Sleep(100);
+                    }
+                }
+            });
+        }
+
+        private void ReportProgress(Progress progress)
+        {
+            this.ProgressPercent = progress.ProgressPercent;
+            this.ProgressText = progress.ProgressText;
+        }
+
+        #endregion
+
+        #region Commands
+
+        private ICommand _CancelCommand;
+
+        public ICommand CancelCommand
+        {
+            get
+            {
+                if (_CancelCommand == null)
+                {
+                    _CancelCommand = new RelayCommand(CancelCommandExecute, CancelCommandCanExecute);
+                }
+                return _CancelCommand;
+            }
+        }
+
+        private bool CancelCommandCanExecute()
+        {
+            return _IsBusy;
+        }
+
+        private void CancelCommandExecute()
+        {
+            DoCancel();
+        }
+
+        private ICommand _ExitCommand;
+
+        public ICommand ExitCommand
+        {
+            get
+            {
+                if (_ExitCommand == null)
+                {
+                    _ExitCommand = new RelayCommand(ExitCommandExecute, ExitCommandCanExecute);
+                }
+                return _ExitCommand;
+            }
+        }
+
+        private bool ExitCommandCanExecute()
+        {
+            return !_IsBusy;
+        }
+
+        private void ExitCommandExecute()
+        {
+            App.Current.Shutdown();
+        }
+
+        private ICommand _StartCommand;
+
+        public ICommand StartCommand
+        {
+            get
+            {
+                if (_StartCommand == null)
+                {
+                    _StartCommand = new RelayCommand(StartCommandExecute, StartCommandCanExecute);
+                }
+                return _StartCommand;
+            }
+        }
+
+        private bool StartCommandCanExecute()
+        {
+            return !_IsBusy;
+        }
+
+        private void StartCommandExecute()
+        {
+            DoStart();
+        }
+
+        #endregion
     }
 }
